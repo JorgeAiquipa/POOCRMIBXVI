f91a1d020b88d8c37da2ffc1517eb5ae06e91ece
Jorge Aiquipa <Jorge Aiquipa@PC_AIQUIPA>
2012-11-19 23:30:28 -0500

    Subiendo

---
+++ POOCRMIBXVI/src/poocrmibxvi/BusinessException.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poocrmibxvi;
+
+/**
+ *
+ * @author Jorge Aiquipa
+ */
+public class BusinessException extends Exception {
+    public BusinessException(String message) {
+        super(message);
+    }
+}
+
