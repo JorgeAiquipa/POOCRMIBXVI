0f627b78a4f47efe238709f4f829d32873da2a19
Jorge Aiquipa <Jorge Aiquipa@PC_AIQUIPA>
2012-11-19 23:11:28 -0500

    Creando proyecto en Java

---
+++ POOCRMIBXVI/src/poocrmibxvi/POOCRMIBXVI.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poocrmibxvi;
+
+/**
+ *
+ * @author Jorge Aiquipa
+ */
+public class POOCRMIBXVI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
