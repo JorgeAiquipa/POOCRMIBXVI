97d66433009dcc2e3508495695e9659d1200919a
praimus <praimus@POSHITOS>
2012-11-22 15:29:38 -0500

    Subiendo la clase Compra

--- POOCRMIBXVI/src/poocrmibxvi/GrupoEstudioManager.java
+++ POOCRMIBXVI/src/poocrmibxvi/GrupoEstudioManager.java
@@ -1,6 +1,8 @@
 package poocrmibxvi;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class GrupoEstudioManager {    
     private ArrayList<GrupoEstudio> grupos;
@@ -37,10 +39,26 @@ public class GrupoEstudioManager {
         return null;
     }
     
-    public void registrar(String nombre, String descripcion, String academia, String curso, String fecha_inicio, String fecha_fin, String estado, String instructores, String link, String local, String aula, String coordenadas) throws BusinessException {
+    public void altaGrupo(String nombre, String descripcion, String academia, String curso, String fecha_inicio, String fecha_fin, String estado, String instructores, String link, String local, String aula, String coordenadas) throws BusinessException {
         validarDatos(nombre, fecha_inicio, fecha_fin);
         validaDuplicidad(nombre);
         GrupoEstudio grupo = new GrupoEstudio(nombre, descripcion, academia, curso, fecha_inicio, fecha_fin, estado, instructores, link, local, aula, coordenadas);
         grupos.add(grupo);
+    }
+    
+    public void filtroVentas(String nombre, String academia, String curso, String fecha_inicio, String fecha_fin, String estado){    
+        // Busqueda secuencial por strings
+        for(GrupoEstudio grupo : grupos){
+            if(grupo.getNombre().compareTo(nombre) == 0)
+                 listar.add(grupo);
+        }
+        //ordenamos la lista por fecha de vencimiento
+        Collections.sort(listar, new Comparator() {    
+            public int compare(Object o1, Object o2) {  
+                GrupoEstudio c1 = (GrupoEstudio) o1;
+                GrupoEstudio c2 = (GrupoEstudio) o2;
+                return c1.getFecha_inicio().compareToIgnoreCase(c2.getFecha_inicio());  
+            }  
+        });      
     }
 }
